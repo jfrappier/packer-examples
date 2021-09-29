3189e578fcd1fadfc32891ab92cedf99d81af925
Jonathan Frappier <jfrappier@users.noreply.github.com>
2021-09-29 15:36:51 -0400

    Add sysprep script for AWS instance
    Adds necessary ec2launch commands for AWS Windows based AMIs

---
+++ resources/windows_aws_sysprep.ps1
@@ -0,0 +1,2 @@
+powershell.exe C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
+powershell.exe C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\SysprepInstance.ps1 -NoShutdown
